a1cc8208080f6ff36f6ae6d94e05ca5a706799fa
David <ddoradoc@minsait.com>
2023-02-01 15:31:41 +0100

    first commit

---
+++ src/main/java/com/indra/notesspringboot/NotesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.indra.notesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesSpringBootApplication.class, args);
+	}
+
+}
